520a15186d8e2ad3c035dff701cccb54f07d0fa3
dnotrad <lentsd@bk.ru>
2022-05-11 23:13:33 +0300

    added dist

---
+++ dist/helpers.d.ts
@@ -0,0 +1,214 @@
+import { IChain } from 'interface';
+export declare const parameters: {
+    chainIDMap: {
+        1: string;
+        3: string;
+        4: string;
+        5: string;
+        42: string;
+        128: string;
+        256: string;
+        69: number;
+        24: number;
+        56: string;
+        97: string;
+        137: string;
+        80001: string;
+        43113: string;
+        43114: string;
+        42220: string;
+        44787: string;
+    };
+    chainsMap: {
+        '0x1': {
+            name: string;
+            chainID: number;
+        };
+        '0x3': {
+            name: string;
+            chainID: number;
+        };
+        '0x4': {
+            name: string;
+            chainID: number;
+        };
+        '0x5': {
+            name: string;
+            chainID: number;
+        };
+        '0x2a': {
+            name: string;
+            chainID: number;
+        };
+        '0x80': {
+            name: string;
+            chainID: number;
+        };
+        '0x100': {
+            name: string;
+            chainID: number;
+        };
+        69: {
+            name: string;
+            chainID: number;
+        };
+        24: {
+            name: string;
+            chainID: number;
+        };
+        '0x38': {
+            name: string;
+            chainID: number;
+        };
+        '0x61': {
+            name: string;
+            chainID: number;
+        };
+        '0x89': {
+            name: string;
+            chainID: number;
+        };
+        '0x13881': {
+            name: string;
+            chainID: number;
+        };
+        '0xa86a': {
+            name: string;
+            chainID: number;
+        };
+        '0xa869': {
+            name: string;
+            chainID: number;
+        };
+        '0xa4ec': {
+            name: string;
+            chainID: number;
+        };
+        '0xaef3': {
+            name: string;
+            chainID: number;
+        };
+    };
+};
+export declare const codeMap: {
+    1: {
+        type: string;
+        name: string;
+    };
+    2: {
+        type: string;
+        name: string;
+    };
+    3: {
+        type: string;
+        name: string;
+    };
+    4: {
+        type: string;
+        name: string;
+    };
+    5: {
+        type: string;
+        name: string;
+    };
+    6: {
+        type: string;
+        name: string;
+    };
+    7: {
+        type: string;
+        name: string;
+    };
+};
+export declare const getCode: (code: number) => any;
+export declare const addChains: (chains: IChain[]) => {
+    chainIDMap: {
+        1: string;
+        3: string;
+        4: string;
+        5: string;
+        42: string;
+        128: string;
+        256: string;
+        69: number;
+        24: number;
+        56: string;
+        97: string;
+        137: string;
+        80001: string;
+        43113: string;
+        43114: string;
+        42220: string;
+        44787: string;
+    };
+    chainsMap: {
+        '0x1': {
+            name: string;
+            chainID: number;
+        };
+        '0x3': {
+            name: string;
+            chainID: number;
+        };
+        '0x4': {
+            name: string;
+            chainID: number;
+        };
+        '0x5': {
+            name: string;
+            chainID: number;
+        };
+        '0x2a': {
+            name: string;
+            chainID: number;
+        };
+        '0x80': {
+            name: string;
+            chainID: number;
+        };
+        '0x100': {
+            name: string;
+            chainID: number;
+        };
+        69: {
+            name: string;
+            chainID: number;
+        };
+        24: {
+            name: string;
+            chainID: number;
+        };
+        '0x38': {
+            name: string;
+            chainID: number;
+        };
+        '0x61': {
+            name: string;
+            chainID: number;
+        };
+        '0x89': {
+            name: string;
+            chainID: number;
+        };
+        '0x13881': {
+            name: string;
+            chainID: number;
+        };
+        '0xa86a': {
+            name: string;
+            chainID: number;
+        };
+        '0xa869': {
+            name: string;
+            chainID: number;
+        };
+        '0xa4ec': {
+            name: string;
+            chainID: number;
+        };
+        '0xaef3': {
+            name: string;
+            chainID: number;
+        };
+    };
+};
